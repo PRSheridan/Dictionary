28e098d507d638b0348a277eb54574930585a1ac
Phil <philrsheridan@gmail.com>
2023-12-02 17:57:27 -0500

    favorites button working, all favorites being added to non-favorites container, working on location for all functions

--- index.js
+++ index.js
@@ -31,14 +31,20 @@ document.addEventListener('DOMContentLoaded', () => {
             let reducedDefinitions = reduceDefinitions(data);
             reducedDefinitions['word'] = wordChoice;
             console.log(reducedDefinitions)
-            buildWord(data, reducedDefinitions)
-
+            buildWord(data, reducedDefinitions, document.getElementById('word-container'))
 //really broken version of updating favorites list- working on fixing might need to refresh on 
 //how to run the local server && how to input my words data (currently not sure if my POSTconfig is good)
 
-            let tempFav = document.getElementById('fav-button')
-            let favList = document.getElementById('fav-container')
-            tempFav.addEventListener('click', () => {
+            let favBtn = document.getElementById('fav-button');
+            let tempWord = {
+                "name": document.getElementById('current-word').textContent,
+                "values": {
+                  "pronunciation": document.getElementById('current-phonetics'),
+                  "definitions": reducedDefinitions,
+                      }   
+                  };
+            buildFavorites(tempWord, reducedDefinitions);
+            favBtn.addEventListener('click', () => {
                 event.preventDefault();
                 const POSTconfig = {
                     method: "POST",
@@ -46,14 +52,8 @@ document.addEventListener('DOMContentLoaded', () => {
                       "Content-Type": "application/json",
                       "Accept": "application/json",
                     },
-                    body: JSON.stringify({
-                      "name": document.getElementById('current-word').textContent,
-                      "values": {
-                        "pronunciation": document.getElementById('current-phonetics'),
-                        "definitions": reducedDefinitions,
-                            }   
-                        })
-                    };
+                    body: JSON.stringify(tempWord)
+                };
                 fetch(favUrl, POSTconfig)
                 .then(function(response) {
                     return response.json();
@@ -62,35 +62,45 @@ document.addEventListener('DOMContentLoaded', () => {
 //NEXT create a buildFavorites function that takes all the posted information about a favorited word and creates a builds a card
 //very likely will need to restructure buildWord to create a cleaner organization of PRE-html data i.e. create an object with
 //all needed information then build the card based on that data rather than individual variables.
-                    favList.appendChild(buildFavorites(data));
+                    buildFavorites(tempWord, reducedDefinitions);
                 });
             });
         });
     });
 });
 
+function buildFavorites(fullData, definitions) {
+    fetch(favUrl, GETconfig)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        console.log(data)
+        buildWord(fullData, definitions, document.getElementById('fav-container'))
+    })
+}
+
 //buildWord is used to display the data retrieved about the word.
 //sub-functions are called for specific tasks: buildDefinitions, buildHistory...
-function buildWord(data, wordData) {
-    let container = document.getElementById('word-container');
+function buildWord(data, definitions, location) {
 
     let newWord = document.createElement('h1');
-    newWord.textContent = `${wordData.word}`;
+    newWord.textContent = `${definitions.word}`;
     newWord.className = 'header-line';
     newWord.id = 'current-word';
-    container.appendChild(newWord);
+    location.appendChild(newWord);
 
     let favButton = document.createElement('button');
     favButton.className = 'header-line';
     favButton.id = 'fav-button';
     favButton.textContent = 'Add to Favorites';
-    container.appendChild(favButton);
+    location.appendChild(favButton);
 
     let phonetics = document.createElement('h2');
     phonetics.className = 'current-phonetics';
     phonetics.textContent = buildPhonetics(data);
-    container.appendChild(phonetics)
-    buildDefinitions(wordData)
+    location.appendChild(phonetics)
+    buildDefinitions(definitions, location)
 };
     
 //function used for gathering all definitions by part of speech and returning an object with keys cooresponding to POS
@@ -111,12 +121,13 @@ function reduceDefinitions (data) {
 }
 
 //constructs lists for each part of speech of wordData
-function buildDefinitions (wordData) {
+function buildDefinitions (wordData, location) {
     for (let index in Object.keys(wordData)) {
         let pos = Object.keys(wordData)[index];
-        if (pos != 'word') { createCollapsible(document.getElementById('word-container'), wordData, pos)
-            let currentDiv = document.getElementById(`${wordData.word}-card-${pos}`);
-            let currentUl = document.getElementById(`${wordData.word}-list-${pos}`);
+        if (pos != 'word') { createCollapsible(wordData, pos, location)
+            console.log(location)
+            let currentDiv = document.getElementById(`${wordData.word}-card-${pos}-${location}`);
+            let currentUl = document.getElementById(`${wordData.word}-list-${pos}-${location}`);
             for (let entry in wordData[pos]) {
                 let definition = wordData[pos][entry].definition;
                 let li = document.createElement('li');
@@ -142,15 +153,15 @@ function buildPhonetics (data) {
 }
 
 //handle button creation for buildDefinitions
-function createCollapsible (location, wordData, pos) {
+function createCollapsible (wordData, pos, location) {
     let div = document.createElement('div');
-    div.id = `${wordData.word}-card-${pos}`;
+    div.id = `${wordData.word}-card-${pos}-${location}`;
     let button = document.createElement('button')
     button.className = 'button';
-    button.id = `${wordData.word}-btn-${pos}`;
+    button.id = `${wordData.word}-btn-${pos}-${location}`;
     button.textContent = pos;
     let ul = document.createElement('ul')
-    ul.id = `${wordData.word}-list-${pos}`;
+    ul.id = `${wordData.word}-list-${pos}-${location}`;
     div.appendChild(button);
     div.appendChild(ul);
     location.appendChild(div);
